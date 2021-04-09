c5493d0d4b937befaf332892c47e44e6bba28688
Andrea Tavakol <andreatavakol@gmail.com>
2021-04-09 02:40:13 -0400

    EDIT: Made adjustments to scripts.js for W8
    but it's still not displaying my stupid table why

--- public/scripts.js
+++ public/scripts.js
@@ -5,7 +5,7 @@ function createTable(json) {
         let tableHead = table.createTHead();
         let row = tableHead.insertRow();
 
-        for (key in json['data'][0]) {
+        for (key in json["data"][0]) {
             //Going to each dining hall's specific property
             let th = document.createElement('th');
             let label = document.createTextNode(key);
@@ -16,11 +16,12 @@ function createTable(json) {
     }
 
     function createTableBody(table) {
-        for (key in json['data']) {
+        for (key in json["data"]) {
             let row = table.insertRow();
-            for (key2 in json['data'][key]) {
+            for (key2 in json["data"][key]) {
+                //Inserting text from each dining hall's specific property
                 let cell = row.insertCell();
-                let text = document.createTextNode(json['data'][key][key2]);
+                let text = document.createTextNode(json["data"][key][key2]);
                 cell.appendChild(text);
             }
         }
@@ -28,9 +29,9 @@ function createTable(json) {
     }
 
     //console.log(json['data'][0]['hall_id'])
-    let table = document.createElement('table');
-    createTableHead(table);
+    let table = document.createElement("table");
     createTableBody(table);
+    createTableHead(table);
     table.setAttribute("class", "table is-bordered");
 }
 
