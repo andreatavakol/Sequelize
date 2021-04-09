080c4a3425285eafea2d27ad8662635c63c51fc4
Andrea Tavakol <andreatavakol@gmail.com>
2021-04-09 01:30:04 -0400

    ADD: Working on W8

--- public/scripts.js
+++ public/scripts.js
@@ -1,29 +1,49 @@
 function createTable(json) {
     if (json == null || json.length === 0) return;
-    
+
+    function createTableHead(table) {
+        let tableHead = table.createTHead();
+        let row = tableHead.insertRow();
+
+        for (key in json['data'][0]) {
+            //Going to each dining hall's specific property
+            let th = document.createElement('th');
+            let label = document.createTextNode(key);
+            th.appendChild(label);
+            row.appendChild(th);
+        }
+        console.log('done making table head');
+    }
+
+    function createTableBody(table) {
+        for (key in json['data']) {
+            let row = table.insertRow();
+            for (key2 in json['data'][key]) {
+                let cell = row.insertCell();
+                let text = document.createTextNode(json['data'][key][key2]);
+                cell.appendChild(text);
+            }
+        }
+        console.log('done making table body');
+    }
+
+    //console.log(json['data'][0]['hall_id'])
     let table = document.createElement('table');
-    let tableHead = document.createElement('thead');
-
-    console.log(Object.keys(json));
-
-    /*for (let key of json) {
-        console.log(json[key]);
-    }*/
-
-
+    createTableHead(table);
+    createTableBody(table);
     table.setAttribute("class", "table is-bordered");
 }
 
 async function windowActions() {
     console.log('page loaded');
     
-    const request = await fetch('/api/meals');
+    const request = await fetch('/api/dining');
     const json = await request.json();
 
     let jsonobj = json;
-    console.table(jsonobj);
 
     createTable(jsonobj);
+    console.log('done everything');
 }
 
 window.onload = windowActions;
