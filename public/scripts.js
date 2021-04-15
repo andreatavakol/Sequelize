60f9f1d95ce470e5c5cb6cbb897b4719de75e411
Andrea Tavakol <andreatavakol@gmail.com>
2021-04-14 23:27:07 -0400

    ADD: Made pickTen function for W10

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,30 @@
+function pickTen(json) {
+    if (json == null || json.length === 0) return;
+
+    const result = [];
+    //i don't know why it spits out 10 results when i set it to i < 20
+    //but i'm just going to let sleeping dogs lie
+    for (let i = 0; i < 20; i++) {
+        const random = Math.floor(Math.random() * json.length);
+        if (result.indexOf(json[random]) !== -1) continue;
+        result.push(json[random]);
+        i++;
+    }
+
+    return result;
+}
+
+function createChart(json1, json2) {
+    if (json1 == null || json1.length === 0 || json2 == null || json2.length === 0) return;
+    //json1 are the main objects we care about
+    //json2 are the values of each thing in json1
+    console.log(json1);
+    console.log(json2);
+
+    const tenMeals = pickTen(json1);
+    console.table(tenMeals);
+}
+
 function createTable(json) {
     if (json == null || json.length === 0) return;
 
@@ -39,12 +66,23 @@ function createTable(json) {
 async function windowActions() {
     console.log('page loaded');
     
-    const request = await fetch('/api/dining');
-    const json = await request.json();
+    const diningRequest = await fetch('/api/dining');
+    const diningJson = await diningRequest.json();
 
-    let jsonobj = json;
+    let diningObj = diningJson;
 
-    createTable(jsonobj);
+    const mealsRequest = await fetch('/api/meals');
+    const mealsJson = await mealsRequest.json();
+
+    let mealsObj = mealsJson;
+
+    const macrosRequest = await fetch('/api/macros');
+    const macrosJson = await macrosRequest.json();
+
+    let macrosObj = macrosJson;
+
+    createChart(mealsObj, macrosObj);
+    createTable(diningObj);
     console.log('done everything');
 }
 
